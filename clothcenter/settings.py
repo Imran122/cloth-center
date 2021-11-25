935c46940f9bc9c9500b9ef04f0cc3ba33c567b4
ghost <imransardar122@gmail.com>
2021-11-26 02:06:21 +0600

    first commit3

--- clothcenter/settings.py
+++ clothcenter/settings.py
@@ -106,13 +106,17 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
 
 USE_I18N = True
 
 USE_L10N = True
 
+# enable time zone support
 USE_TZ = True
+
+# select a timezone
+TIME_ZONE = 'Europe/Rome'
 
 
 # Static files (CSS, JavaScript, Images)
